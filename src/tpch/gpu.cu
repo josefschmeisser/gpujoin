a601951eb430f311216854085c0a623550c1f0eb
Josef Schmeißer <josef.schmeisser@tum.de>
2020-10-13 16:48:36 +0200

    cleanup

--- src/tpch/gpu.cu
+++ src/tpch/gpu.cu
@@ -445,42 +445,6 @@ int main(int argc, char** argv) {
     // Set a heap size of 128 megabytes. Note that this must
     // be done before any kernel is launched.
     cudaThreadSetLimit(cudaLimitMallocHeapSize, 1024*1024*1024);
-#if 0
-    mallocTest<<<1, 5>>>();
-    cudaThreadSynchronize();
-    return 0;
-
-    int N = 1<<20;
-    float *x, *y;
-
-    // Allocate Unified Memory -- accessible from CPU or GPU
-    cudaMallocManaged(&x, N*sizeof(float));
-    cudaMallocManaged(&y, N*sizeof(float));
-
-    // initialize x and y arrays on the host
-    for (int i = 0; i < N; i++) {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
-    }
-
-    // Launch kernel on 1M elements on the GPU
-    int blockSize = 256;
-    int numBlocks = (N + blockSize - 1) / blockSize;
-    add<<<numBlocks, blockSize>>>(N, x, y);
-
-    // Wait for GPU to finish before accessing on host
-    cudaDeviceSynchronize();
-
-    // Check for errors (all values should be 3.0f)
-    float maxError = 0.0f;
-    for (int i = 0; i < N; i++)
-        maxError = fmax(maxError, fabs(y[i]-3.0f));
-    std::cout << "Max error: " << maxError << std::endl;
-
-    // Free memory
-    cudaFree(x);
-    cudaFree(y);
-#endif
 
     std::memset(globalHT, 0, 16*sizeof(void*));
     std::memset(sorted, 0, 16*sizeof(void*));
