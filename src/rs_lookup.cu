c490f2c5bf9f2d7807197ef9d20e511349bfc805
Josef Schmeißer <josef.schmeisser@tum.de>
2020-12-02 10:58:41 +0100

    [wip] radixspline lookup

--- src/rs_lookup.cu
+++ src/rs_lookup.cu
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdint>
 #include <cstdio>
 #include <bits/c++config.h>
@@ -8,6 +9,7 @@
 #include <numeric>
 #include <chrono>
 #include <cstring>
+#include <stdexcept>
 
 #include "rs/multi_map.h"
 #include "btree.hpp"
@@ -19,7 +21,8 @@ using rs_rt_entry_t = uint32_t;
 using rs_spline_point_t = rs::Coord<rs_key_t>;
 using payload_t = btree::payload_t;
 
-static constexpr unsigned numElements = 1e2;
+static constexpr int device_id = 0;
+static constexpr unsigned numElements = 1e8;
 static constexpr payload_t invalidTid = std::numeric_limits<payload_t>::max();
 
 struct Relation {
@@ -85,14 +88,7 @@ template<typename T1, typename T2, typename P>
 __device__ unsigned lower_bound(const T1& key, const T2* arr, const unsigned size, P cmp) {
     unsigned lower = 0;
     unsigned count = size;
-    while (count > 0) {/*
-        unsigned mid = count / 2;
-        if (cmp(arr[mid], key)) {
-            lower = mid + 1;
-            count -= mid + 1;
-        } else {
-            count = mid;
-        }*/
+    while (count > 0) {
         unsigned step = count / 2;
         unsigned mid = lower + step;
         if (cmp(arr[mid], key)) {
@@ -111,85 +107,31 @@ __global__ void do_lower_bound(const int* arr, const unsigned size) {
     });
 }
 
-/*
-  // Returns the index of the spline point that marks the end of the spline segment that contains the `key`:
-  // `key` ∈ (spline[index - 1], spline[index]]
-  size_t GetSplineSegment(const KeyType key) const {
-    // Narrow search range using radix table.
-    const KeyType prefix = (key - min_key_) >> num_shift_bits_;
-    assert (prefix + 1 < radix_table_.size());
-    const uint32_t begin = radix_table_[prefix];
-    const uint32_t end = radix_table_[prefix + 1];
-
-    if (end - begin < 32) {
-      // Do linear search over narrowed range.
-      uint32_t current = begin;
-      while (spline_points_[current].x < key) ++current;
-      return current;
-    }
-
-    // Do binary search over narrowed range.
-    const auto lb = std::lower_bound(spline_points_.begin() + begin,
-                                     spline_points_.begin() + end,
-                                     key,
-                                     [](const Coord<KeyType>& coord, const KeyType key) { return coord.x < key; });
-    return std::distance(spline_points_.begin(), lb);
-  }
-*/
 __device__ unsigned get_spline_segment(ManagedRadixSpline* rs, const rs_key_t key) {
-    printf("get_spline_segment: %lu\n", key);
-
     const rs_key_t prefix = (key - rs->min_key_) >> rs->num_shift_bits_;
-    printf("key: %lu prefix: %lu\n", key, prefix);
 
     const uint32_t begin = rs->radix_table_[prefix];
     const uint32_t end = rs->radix_table_[prefix + 1];
 
     // TODO measure linear search for narrow ranges as in the reference implementation
 
-
     const auto range_size = end - begin;
     const auto lb = begin + lower_bound(key, rs->spline_points_ + begin, range_size, [] (const rs_spline_point_t& coord, const rs_key_t key) {
         return coord.x < key;
     });
-    printf("key: %lu, lb: %u\n", key, lb);
+//    printf("key: %lu, lb: %u\n", key, lb);
     return lb;
 }
 
-/*
-  // Returns the estimated position of `key`.
-  double GetEstimatedPosition(const KeyType key) const {
-    // Truncate to data boundaries.
-    if (key <= min_key_) return 0;
-    if (key >= max_key_) return num_keys_ - 1;
-
-    // Find spline segment with `key` ∈ (spline[index - 1], spline[index]].
-    const size_t index = GetSplineSegment(key);
-    const Coord<KeyType> down = spline_points_[index - 1];
-    const Coord<KeyType> up = spline_points_[index];
-
-    // Compute slope.
-    const double x_diff = up.x - down.x;
-    const double y_diff = up.y - down.y;
-    const double slope = y_diff / x_diff;
-
-    // Interpolate.
-    const double key_diff = key - down.x;
-    return std::fma(key_diff, slope, down.y);
-  }
-*/
-
 __device__ double get_estimate(ManagedRadixSpline* rs, const rs_key_t key) {
-//TODO check: these two statements should not be required
     if (key <= rs->min_key_) return 0;
     if (key >= rs->max_key_) return rs->num_keys_ - 1;
 
     // find spline segment
     const unsigned index = get_spline_segment(rs, key);
-//printf("index: %u\n", index);
     const rs_spline_point_t& down = rs->spline_points_[index - 1];
     const rs_spline_point_t& up = rs->spline_points_[index];
-//printf("point: %f\n", down.x);
+
     // slope
     const double x_diff = up.x - down.x;
     const double y_diff = up.y - down.y;
@@ -200,41 +142,24 @@ __device__ double get_estimate(ManagedRadixSpline* rs, const rs_key_t key) {
     return key_diff * slope + down.y;
 }
 
-/*
-  // Returns a search bound [begin, end) around the estimated position.
-  SearchBound GetSearchBound(const KeyType key) const {
-    const size_t estimate = GetEstimatedPosition(key);
-    const size_t begin = (estimate < max_error_) ? 0 : (estimate - max_error_);
-    // `end` is exclusive.
-    const size_t end = (estimate + max_error_ + 2 > num_keys_) ? num_keys_ : (estimate + max_error_ + 2);
-    return SearchBound{begin, end};
-  }
-*/
 __device__ payload_t rs_lookup(ManagedRadixSpline* rs, const rs_key_t key, const Relation& rel) {
-//    printf("key: %lu\n", key);
     const unsigned estimate = get_estimate(rs, key);
-    printf("key: %lu estimate: %u\n", key, estimate);
+//    printf("key: %lu estimate: %u\n", key, estimate);
     const unsigned begin = (estimate < rs->max_error_) ? 0 : (estimate - rs->max_error_);
     const unsigned end = (estimate + rs->max_error_ + 2 > rs->num_keys_) ? rs->num_keys_ : (estimate + rs->max_error_ + 2);
-    printf("key: %lu search bound [%u, %u)\n", key, begin, end);
-
 
     const auto bound_size = end - begin;
-    //printf("bound size: %d\n", bound_size);
     const unsigned pos = begin + lower_bound(key, &rel.pk[begin], bound_size, [] (const rs_key_t& a, const rs_key_t& b) -> int {
-        //printf("a: %lu b: %lu\n", a, b);
         return a < b;
     });
-int index = blockIdx.x * blockDim.x + threadIdx.x;
-printf("i: %d rel.pk[i]: %lu\n", index, rel.pk[index]);
-    printf("key: %lu search bound [%u, %u) pos: %u expected: %d\n", key, begin, end, pos, index);
+//    int index = blockIdx.x * blockDim.x + threadIdx.x;
+//    printf("key: %lu search bound [%u, %u) pos: %u expected: %d\n", key, begin, end, pos, index);
     return (pos < rel.count) ? reinterpret_cast<payload_t>(pos) : invalidTid;
 }
 
-__global__ void rs_bulk_lookup(ManagedRadixSpline* rs, unsigned n, rs_key_t* keys, Relation rel, payload_t* tids) {
+__global__ void rs_bulk_lookup(ManagedRadixSpline* rs, unsigned n, const rs_key_t* keys, Relation rel, payload_t* tids) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
-//    printf("index %d\n", index);
     for (int i = index; i < n; i += stride) {
         tids[i] = rs_lookup(rs, keys[i], rel);
     }
@@ -257,15 +182,6 @@ int main(int argc, char** argv) {
 
         auto rs = builRadixSpline(keys);
 
-    /*
-        // Search using RadixSpline.
-        rs::SearchBound bound = rs.GetSearchBound(8128);
-        cout << "The search key is in the range: ["
-            << bound.begin << ", " << bound.end << ")" << endl;
-        auto start = begin(keys) + bound.begin, last = begin(keys) + bound.end;
-        cout << "The key is at position: " << std::lower_bound(start, last, 8128) - begin(keys) << endl;
-    */
-
         RawRadixSpline* rrs = reinterpret_cast<RawRadixSpline*>(&rs);
         cudaMallocManaged(&mrs, sizeof(ManagedRadixSpline));
         std::memcpy(mrs, &rs, sizeof(ManagedRadixSpline));
@@ -285,15 +201,15 @@ int main(int argc, char** argv) {
 
         printf("radix table size: %lu\n", rrs->radix_table_.size());
 
-        cudaMalloc(&lookupKeys, numElements*sizeof(rs_key_t));
+        cudaMallocManaged(&lookupKeys, keys_size);
         // TODO shuffle keys/Zipfian lookup patterns
-        cudaMemcpy(lookupKeys, keys.data(), numElements*sizeof(rs_key_t), cudaMemcpyHostToDevice);
+        cudaMemcpy(lookupKeys, keys.data(), keys_size, cudaMemcpyHostToDevice);
+        cudaMemAdvise(lookupKeys, keys_size, cudaMemAdviseSetReadMostly, device_id);
+        cudaMemPrefetchAsync(lookupKeys, keys_size, device_id);
     }
 
     btree::payload_t* tids;
     cudaMallocManaged(&tids, numElements*sizeof(decltype(tids)));
-
-
 
     int blockSize = 32;
     int numBlocks = (numElements + blockSize - 1) / blockSize;
@@ -308,7 +224,8 @@ int main(int argc, char** argv) {
     std::cout << "GPU MOps: " << (numElements/1e6)/(kernelTime/1e3) << endl;
 
     for (unsigned i = 0; i < numElements; ++i) {
-        printf("tid: %lu\n", reinterpret_cast<uint64_t>(tids[i]));
+        size_t pos = reinterpret_cast<size_t>(tids[i]);
+        if (lookupKeys[i] != rel.pk[pos]) throw std::runtime_error("key mismatch");
     }
 
     return 0;
