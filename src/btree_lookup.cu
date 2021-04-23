dbd5eb6ae353fcb70330aeb6093785a69c810ddc
Josef Schmeißer <josef.schmeisser@tum.de>
2021-04-23 09:42:33 +0200

    decouple build and lookup

--- src/btree_lookup.cu
+++ src/btree_lookup.cu
@@ -15,7 +15,9 @@
 using namespace std;
 
 static constexpr unsigned maxRepetitions = 10;
-static unsigned numElements = 1e8;
+static constexpr unsigned activeLanes = 32;
+static constexpr unsigned numLookups = 1e7;
+static unsigned numElements = 1e7;
 
 using namespace btree;
 using namespace btree::cuda;
@@ -37,13 +39,13 @@ __global__ void btree_bulk_lookup(const Node* __restrict__ tree, unsigned n, con
     }
 }
 
-
+/*
 __global__ void btree_bulk_lookup_serialized(const Node* __restrict__ tree, unsigned n, const btree::key_t* __restrict__ keys, payload_t* __restrict__ tids) {
     enum { MAX_ACTIVE = 16, RUNS = 32 / MAX_ACTIVE };
 
     const int lane_id = threadIdx.x % 32;
     const int lane_mask = 1<<lane_id;
-    
+
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
 
@@ -57,6 +59,27 @@ __global__ void btree_bulk_lookup_serialized(const Node* __restrict__ tree, unsi
                 //tids[i] = btree::cuda::btree_lookup_with_hints(tree, keys[i]);
             }
             active_lanes <<= MAX_ACTIVE;
+        }
+    }
+}
+*/
+
+template<unsigned MAX_ACTIVE>
+__global__ void btree_bulk_lookup_serialized(const Node* __restrict__ tree, unsigned n, const btree::key_t* __restrict__ keys, payload_t* __restrict__ tids) {
+    //enum { MAX_ACTIVE = 16, RUNS = 32 / MAX_ACTIVE };
+
+    const int lane_id = threadIdx.x % 32;
+    const uint32_t lane_mask = 1<<lane_id;
+    const uint32_t active_lanes = (1<<MAX_ACTIVE) - 1; // mask of active lanes
+
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = index; i < n; i += stride) {
+        //printf("ative mask: %d\n", active_lanes);
+        if (active_lanes & lane_mask) {
+            tids[i] = btree_lookup(tree, keys[i]);
+            //tids[i] = btree::cuda::btree_lookup_with_hints(tree, keys[i]);
         }
     }
 }
@@ -80,50 +103,64 @@ int main(int argc, char** argv) {
         if (!found) throw 0;
     }
 
+    // shuffle keys
+    auto rng = std::default_random_engine {};
+    std::shuffle(std::begin(keys), std::end(keys), rng);
+
+    // calculate a factory by which the number of lookups has to be scaled in order to be able to serve all threads
+    int lookupFactor = 32/activeLanes + ((32%activeLanes > 0) ? 1 : 0);
+    const auto numAugmentedLookups = numLookups*lookupFactor;
+    std::cout << "lookup scale factor: " << lookupFactor << " numAugmentedLookups: " << numAugmentedLookups << std::endl;
+
+    // generate lookup keys
+    std::vector<btree::key_t> lookupKeys(numAugmentedLookups);
+    std::iota(lookupKeys.begin(), lookupKeys.end(), 0);
+    std::shuffle(std::begin(lookupKeys), std::end(lookupKeys), rng);
+    // TODO zipfian lookup patterns
+
+    // copy lookup keys
+    btree::key_t* d_lookupKeys;
+    cudaMalloc(&d_lookupKeys, numAugmentedLookups*sizeof(btree::key_t));
+    cudaMemcpy(d_lookupKeys, lookupKeys.data(), numAugmentedLookups*sizeof(btree::key_t), cudaMemcpyHostToDevice);
+    btree::payload_t* d_tids;
+    cudaMalloc(&d_tids, numAugmentedLookups*sizeof(decltype(d_tids)));
+
+    //btree::prefetchTree(tree, 0);
+    auto d_tree = tree;
+    d_tree = btree::copy_btree_to_gpu(tree);
+
     int blockSize = 512;
-    int numBlocks = (numElements + blockSize - 1) / blockSize;
+    int numBlocks = (numAugmentedLookups + blockSize - 1) / blockSize;
     /*
     int numSMs;
     cudaDeviceGetAttribute(&numSMs, cudaDevAttrMultiProcessorCount, 0);
     int numBlocks = 32*numSMs;*/
     printf("numblocks: %d\n", numBlocks);
 
-    // shuffle keys
-    auto rng = std::default_random_engine {};
-    std::shuffle(std::begin(keys), std::end(keys), rng);
-    // TODO zipfian lookup patterns
-
-    btree::key_t* lookupKeys;
-    cudaMalloc(&lookupKeys, numElements*sizeof(btree::key_t));
-    // TODO shuffle keys/Zipfian lookup patterns
-    cudaMemcpy(lookupKeys, keys.data(), numElements*sizeof(btree::key_t), cudaMemcpyHostToDevice);
-    btree::payload_t* d_tids;
-    cudaMalloc(&d_tids, numElements*sizeof(decltype(d_tids)));
-
-    //btree::prefetchTree(tree, 0);
-    auto d_tree = tree;
-    d_tree = btree::copy_btree_to_gpu(tree);
-
+    std::cout << "active lanes: " << activeLanes << std::endl;
     printf("executing kernel...\n");
     const auto kernelStart = std::chrono::high_resolution_clock::now();
     for (unsigned rep = 0; rep < maxRepetitions; ++rep) {
-        btree_bulk_lookup_serialized<<<numBlocks, blockSize>>>(d_tree, numElements, lookupKeys, d_tids);
+        btree_bulk_lookup_serialized<activeLanes><<<numBlocks, blockSize>>>(d_tree, numAugmentedLookups, d_lookupKeys, d_tids);
         cudaDeviceSynchronize();
     }
     const auto kernelStop = std::chrono::high_resolution_clock::now();
     const auto kernelTime = chrono::duration_cast<chrono::microseconds>(kernelStop - kernelStart).count()/1000.;
     std::cout << "Kernel time: " << kernelTime << " ms\n";
-    std::cout << "GPU MOps: " << (maxRepetitions*numElements/1e6)/(kernelTime/1e3) << endl;
+    std::cout << "GPU MOps: " << (maxRepetitions*numLookups/1e6)/(kernelTime/1e3) << endl;
 
-    // validate results
-    printf("validating results...\n");
-    std::unique_ptr<btree::payload_t[]> h_tids(new btree::payload_t[numElements]);
-    cudaMemcpy(h_tids.get(), d_tids, numElements*sizeof(decltype(d_tids)), cudaMemcpyDeviceToHost);
-    for (unsigned i = 0; i < numElements; ++i) {
-        //printf("tid: %lu key[i]: %lu\n", reinterpret_cast<uint64_t>(h_tids[i]), keys[i]);
-        if (reinterpret_cast<uint64_t>(h_tids[i]) != keys[i]) {
-            printf("i: %u tid: %lu key[i]: %u\n", i, reinterpret_cast<uint64_t>(h_tids[i]), keys[i]);
-            throw;
+    // TODO: implement
+    if constexpr (activeLanes == 32) {
+        // validate results
+        printf("validating results...\n");
+        std::unique_ptr<btree::payload_t[]> h_tids(new btree::payload_t[numElements]);
+        cudaMemcpy(h_tids.get(), d_tids, numElements*sizeof(decltype(d_tids)), cudaMemcpyDeviceToHost);
+        for (unsigned i = 0; i < numElements; ++i) {
+            //printf("tid: %lu key[i]: %lu\n", reinterpret_cast<uint64_t>(h_tids[i]), keys[i]);
+            if (reinterpret_cast<uint64_t>(h_tids[i]) != keys[i]) {
+                printf("i: %u tid: %lu key[i]: %u\n", i, reinterpret_cast<uint64_t>(h_tids[i]), keys[i]);
+                throw;
+            }
         }
     }
 
