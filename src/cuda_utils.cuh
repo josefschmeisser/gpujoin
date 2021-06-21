0ce27de93cb70d30be313bba5cf6dc6cf3249f0e
Josef Schmeißer <josef.schmeisser@tum.de>
2021-06-21 10:46:43 +0200

    add utility functions

--- src/cuda_utils.cuh
+++ src/cuda_utils.cuh
@@ -7,4 +7,22 @@ __forceinline__ __device__ unsigned lane_id() {
     return ret;
 }
 
+template<class T>
+__device__ T atomic_sub_safe(T* address, T val) {
+    unsigned expected, update, old;
+    old = *address;
+    do {
+        expected = old;
+        update = (old > val) ? (old - val) : 0;
+        old = atomicCAS(address, expected, update);
+    } while (expected != old);
+    return old;
+}
+
+template<class T>
+__forceinline__ __device__ T round_up_pow2(T value) {
+    return static_cast<T>(1) << (sizeof(T)*8 - __clz(value - 1));
+}
+
 #define FULL_MASK 0xffffffff
+#define GPU_CACHE_LINE_SIZE 128
