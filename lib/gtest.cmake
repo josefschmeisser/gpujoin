144abb3bb7d722c9307b5a1d3ebd4efb6ab7c703
Josef Schmeißer <josef.schmeisser@tum.de>
2021-08-06 10:59:00 +0200

    workaround for path inconsistencies between differen cmake versions

--- lib/gtest.cmake
+++ lib/gtest.cmake
@@ -46,8 +46,16 @@ ExternalProject_Add(
 # Prepare gtest
 ExternalProject_Get_Property(gtest_src SOURCE_DIR)
 ExternalProject_Get_Property(gtest_src BINARY_DIR)
+
+# workaround for path inconsistencies between differen cmake versions
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.18.0") # not sure when this breaking change was really introduced
+    set(library_path "${BINARY_DIR}")
+else()
+    cmake_path(APPEND library_path "${BINARY_DIR}" "lib")
+endif()
+
 set(GTEST_INCLUDE_DIR ${SOURCE_DIR}/include )
-set(GTEST_LIBRARY_PATH ${BINARY_DIR}/lib/libgtest.a)
+set(GTEST_LIBRARY_PATH ${library_path}/libgtest.a)
 file(MAKE_DIRECTORY ${GTEST_INCLUDE_DIR})
 
 add_library(gtest STATIC IMPORTED)
@@ -55,15 +63,22 @@ set_property(TARGET gtest PROPERTY IMPORTED_LOCATION ${GTEST_LIBRARY_PATH})
 set_property(TARGET gtest APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${GTEST_INCLUDE_DIR})
 
 add_library(gtest_main STATIC IMPORTED)
-set(GTEST_MAIN_LIBRARY_PATH ${BINARY_DIR}/lib/libgtest_main.a)
+set(GTEST_MAIN_LIBRARY_PATH ${library_path}/libgtest_main.a)
 set_property(TARGET gtest_main PROPERTY IMPORTED_LOCATION ${GTEST_MAIN_LIBRARY_PATH})
 set_property(TARGET gtest_main APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${GTEST_INCLUDE_DIR})
 
 # Prepare gmock
 ExternalProject_Get_Property(gtest_src SOURCE_DIR)
 ExternalProject_Get_Property(gtest_src BINARY_DIR)
+
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.18.0") # not sure when this breaking change was really introduced
+    set(library_path "${BINARY_DIR}")
+else()
+    cmake_path(APPEND library_path "${BINARY_DIR}" "lib")
+endif()
+
 set(GMOCK_INCLUDE_DIR ${SOURCE_DIR}/include)
-set(GMOCK_LIBRARY_PATH ${BINARY_DIR}/lib/libgmock.a)
+set(GMOCK_LIBRARY_PATH ${library_path}/libgmock.a)
 file(MAKE_DIRECTORY ${GMOCK_INCLUDE_DIR})
 add_library(gmock STATIC IMPORTED)
 set_property(TARGET gmock PROPERTY IMPORTED_LOCATION ${GMOCK_LIBRARY_PATH})
