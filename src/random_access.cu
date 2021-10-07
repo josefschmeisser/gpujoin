7be4a2f1dc5f052d6fad4d6a412456998889a1de
Josef Schmeißer <josef.schmeisser@tum.de>
2021-10-07 16:23:49 +0200

    update random access benchmark

--- src/random_access.cu
+++ src/random_access.cu
@@ -11,17 +11,23 @@
 #include <random>
 #include <algorithm>
 
+#include "allocators.hpp"
+#include "device_array.hpp"
 #include "utils.hpp"
 
 using namespace std;
 
-static constexpr unsigned accessRounds = 1;
-static constexpr unsigned maxRepetitions = 10;
+static constexpr unsigned repetitions = 10;
+static constexpr unsigned blockSize = 512;
 static unsigned numElements = 1e9;
+static unsigned numAccesses = 1e9;
 
 using lookup_t = uint32_t;
-using placement_policy = vector_to_managed_array;
 
+template<class T> using host_allocator_t = std::allocator<T>;
+template<class T> using device_allocator_t = cuda_allocator<T, cuda_allocation_type::zero_copy>;
+
+#if 0
 __global__ void random_access(const lookup_t* __restrict__ src, const lookup_t* __restrict__ lookup, lookup_t* __restrict__ dst, unsigned n, unsigned rounds) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
@@ -33,46 +39,69 @@ __global__ void random_access(const lookup_t* __restrict__ src, const lookup_t* 
         dst[i] = current;
     }
 }
+#endif
+
+__device__ uint32_t hash_key(uint32_t k) {
+    k ^= k >> 16;
+    k *= 0x85ebca6b;
+    k ^= k >> 13;
+    k *= 0xc2b2ae35;
+    k ^= k >> 16;
+    return k;
+}
+
+template<unsigned BLOCK_THREADS>
+__global__ void random_access(const lookup_t* __restrict__ data, unsigned size, unsigned n) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    __shared__ lookup_t results[BLOCK_THREADS];
+    for (int i = index; i < n; i += stride) {
+        auto pos = hash_key(i) & (size - 1);
+        //auto pos = i; // sequential access
+        lookup_t current = data[pos];
+        (void)current;
+        results[threadIdx.x] += current;
+    }
+}
 
 int main(int argc, char** argv) {
-    int blockSize = 512;
-    int numBlocks = (numElements + blockSize - 1) / blockSize;
+    int numBlocks = (numAccesses + blockSize - 1) / blockSize;
+    /*
     if (argc > 1) {
         blockSize = std::atoi(argv[1]);
     }
-    printf("numblocks: %d\n", numBlocks);
+    printf("numblocks: %d\n", numBlocks);*/
 
-    double transfered_gib = accessRounds*numElements*sizeof(lookup_t)/std::pow(1024, 3.);
-    std::cout << "total size: " << transfered_gib << " GiB" << std::endl;
+    printf("numAccesses: %u\n", numAccesses);
+    double transfered_gib = 1.*repetitions*numAccesses*sizeof(lookup_t)/std::pow(1024, 3.);
+    std::cout << "to transfer: " << transfered_gib << " GiB" << std::endl;
 
-    lookup_t* d_src, * d_lookup, * d_dst;
+    std::vector<lookup_t, host_allocator_t<lookup_t>> lookup_data(numElements);
     {
-        std::vector<lookup_t> keys(numElements);
-        std::iota(keys.begin(), keys.end(), 0);
-
+        // generate data
+        std::iota(lookup_data.begin(), lookup_data.end(), 0);
+        /*
         // shuffle keys
         auto rng = std::default_random_engine {};
-        std::shuffle(std::begin(keys), std::end(keys), rng);
-
-        static placement_policy pp;
-        d_src = pp(keys);
-
-        vector_to_device_array f;
-        d_lookup = f(keys);
-
-        cudaMalloc(&d_dst, numElements*sizeof(lookup_t));
+        std::shuffle(std::begin(lookup_data), std::end(lookup_data), rng);
+        */
     }
+
+    // create gpu accessible vectors
+    device_allocator_t<lookup_t> device_allocator;
+    auto d_lookup_data = create_device_array_from(lookup_data, device_allocator);
 
     printf("executing kernel...\n");
     const auto kernelStart = std::chrono::high_resolution_clock::now();
-    for (unsigned rep = 0; rep < maxRepetitions; ++rep) {
-        random_access<<<numBlocks, blockSize>>>(d_src, d_lookup, d_dst, numElements, accessRounds);
+    for (unsigned rep = 0; rep < repetitions; ++rep) {
+        random_access<blockSize><<<numBlocks, blockSize>>>(d_lookup_data.data(), numElements, numAccesses);
         cudaDeviceSynchronize();
     }
     const auto kernelStop = std::chrono::high_resolution_clock::now();
-    const auto kernelTime = chrono::duration_cast<chrono::microseconds>(kernelStop - kernelStart).count()/1000.;
+    const double kernelTime = chrono::duration_cast<chrono::microseconds>(kernelStop - kernelStart).count()/1000.;
     std::cout << "Kernel time: " << kernelTime << " ms\n";
-    std::cout << "GPU MOps: " << (maxRepetitions*numElements/1e6)/(kernelTime/1e3) << endl;
+    std::cout << "GPU MOps: " << (repetitions*numAccesses/1e6)/(kernelTime/1e3) << endl;
 
     double rate = transfered_gib/(kernelTime/1000.);
     std::cout << "transfer rate: " << rate << " GiB/s" << std::endl;
