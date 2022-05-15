f5484b3a9c55acc2c04c638adba20173862400e9
Josef Schmeißer <josef.schmeisser@tum.de>
2022-05-15 23:06:43 +0200

    refactor

--- src/tpch/tpch_14_common.cu
+++ src/tpch/tpch_14_common.cu
@@ -21,22 +21,6 @@
 #include "tpch_14_ij.cuh"
 #include "tpch_14_ij_partitioning.cuh"
 
-index_type_enum parse_index_type(const std::string& index_name) {
-    if (index_name == "btree") {
-        return index_type_enum::btree;
-    } else if (index_name == "harmonia") {
-        return index_type_enum::harmonia;
-    } else if (index_name == "lower_bound") {
-        return index_type_enum::lower_bound;
-    } else if (index_name == "radix_spline") {
-        return index_type_enum::radix_spline;
-    } else if (index_name == "no_op") {
-        return index_type_enum::no_op;
-    } else {
-        throw std::runtime_error("unknown index type");
-    }
-}
-
 struct query_data {
     Database db;
 
