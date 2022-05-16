99f01a90e19aa3c4e4e74e1046b6270619a015e8
Josef Schmeißer <josef.schmeisser@tum.de>
2022-05-16 13:20:39 +0200

    fix

---
+++ src/harmonia.cu
@@ -0,0 +1,10 @@
+#include "harmonia.cuh"
+
+namespace harmonia {
+
+// only contains the upper tree levels; stored in constant memory
+// retain some space for kernel launch arguments (those are also stored in constant memory)
+//__constant__ uint32_t harmonia_upper_levels[14336];
+__constant__ uint32_t harmonia_upper_levels[42*1024/sizeof(uint32_t)];
+
+}
