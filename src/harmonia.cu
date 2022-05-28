2074394d145388bab62e04a8f43cd9feb2a5ca1f
Josef Schmeißer <josef.schmeisser@tum.de>
2022-05-28 22:36:20 +0200

    add compiler directive

--- src/harmonia.cu
+++ src/harmonia.cu
@@ -4,6 +4,10 @@
 
 namespace harmonia {
 
+#ifdef NRDC
+#error This file should not be compiled when rdc is disabled
+#endif
+
 // only contains the upper tree levels; stored in constant memory
 // retain some space for kernel launch arguments (those are also stored in constant memory)
 __constant__ child_ref_t harmonia_upper_levels[harmonia_max_constant_mem/sizeof(child_ref_t)];
