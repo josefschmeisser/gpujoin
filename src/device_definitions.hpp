f4934f3bc6546c6bd076f158dd7dd609a9c8a7d7
Josef Schmeißer <j.m.schmeisser@gmail.com>
2022-09-12 16:08:49 +0200

    use device specific integer type in search routines

---
+++ src/device_definitions.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cstdint>
+
+using device_size_t = uint64_t;
--- src/search.cuh
+++ src/search.cuh
@@ -2,14 +2,15 @@
 
 #include <cassert>
 
+#include "device_definitions.hpp"
 #include "cuda_utils.cuh"
 
 template<class T>
-__device__ unsigned branchy_binary_search(T x, const T* arr, const unsigned size) {
-    unsigned lower = 0;
-    unsigned upper = size;
+__device__ device_size_t branchy_binary_search(T x, const T* arr, const device_size_t size) {
+    device_size_t lower = 0;
+    device_size_t upper = size;
     do {
-        unsigned mid = ((upper - lower) / 2) + lower;
+        device_size_t mid = ((upper - lower) / 2) + lower;
         if (x < arr[mid]) {
             upper = mid;
         } else if (x > arr[mid]) {
@@ -22,15 +23,12 @@ __device__ unsigned branchy_binary_search(T x, const T* arr, const unsigned size
 }
 
 template<class T>
-__device__ unsigned branch_free_binary_search(T x, const T* arr, const unsigned size) {
+__device__ device_size_t branch_free_binary_search(T x, const T* arr, const device_size_t size) {
     if (size < 1) { return 0; }
 
-    const unsigned steps = 31 - __clz(size - 1);
-    //printf("steps: %d\n", steps);
-    unsigned mid = 1 << steps;
-
-    unsigned ret = (arr[mid] < x) * (size - mid);
-    //while (mid > 0) {
+    const unsigned steps = 31u - __clz(size - 1);
+    device_size_t mid = 1 << steps;
+    device_size_t ret = (arr[mid] < x) * (size - mid);
     for (unsigned step = 1; step <= steps; ++step) {
         mid >>= 1;
         ret += (arr[ret + mid] < x) ? mid : 0;
@@ -41,7 +39,7 @@ __device__ unsigned branch_free_binary_search(T x, const T* arr, const unsigned 
 }
 
 template<class T, unsigned max_step = 4> // TODO find optimal limit
-__device__ unsigned branch_free_exponential_search(T x, const T* arr, const unsigned n, const float hint) {
+__device__ device_size_t branch_free_exponential_search(T x, const T* arr, const device_size_t n, const float hint) {
     //if (size < 1) return;
 
     const int last = n - 1;
@@ -51,7 +49,7 @@ __device__ unsigned branch_free_exponential_search(T x, const T* arr, const unsi
     bool cont = true;
     bool less = arr[start] < x;
     int offset = -1 + 2*less;
-    unsigned current = max(0, min(last , start + offset));
+    device_size_t current = max(0, min(last , start + offset));
     for (unsigned i = 0; i < max_step; ++i) {
         cont = ((arr[current] < x) == less);
         offset = cont ? offset<<1 : offset;
@@ -60,39 +58,39 @@ __device__ unsigned branch_free_exponential_search(T x, const T* arr, const unsi
 
     const auto pre_lower = max(0, min(static_cast<int>(n), start + (offset>>less)));
     const auto pre_upper = 1 + max(0, min(static_cast<int>(n), start + (offset>>(1 - less))));
-    const unsigned lower = (!cont || less) ? pre_lower : 0;
-    const unsigned upper = (!cont || !less) ? pre_upper : n;
+    const device_size_t lower = (!cont || less) ? pre_lower : 0;
+    const device_size_t upper = (!cont || !less) ? pre_upper : n;
 
     return lower + branchy_binary_search(x, arr + lower, upper - lower); // TODO measure alternatives
     //return lower + branch_free_binary_search(x, arr + lower, upper - lower); // TODO measure alternatives
 }
 
 template<class T>
-__device__ unsigned exponential_search(T x, const T* arr, const unsigned size) {
+__device__ device_size_t exponential_search(T x, const T* arr, const device_size_t size) {
     assert(size > 0);
-    int bound = 1;
+    device_size_t bound = 1;
     while (bound < size && arr[bound] < x) {
         bound <<= 1;
     }
-    const auto lower = bound>>1;
+    const device_size_t lower = bound>>1;
     return lower + branchy_binary_search(x, arr + lower, min(bound + 1, size - lower));
 }
 
 template<class T>
-__device__ unsigned linear_search(T x, const T* arr, const unsigned size) {
-    for (unsigned i = 0; i < size; ++i) {
+__device__ device_size_t linear_search(T x, const T* arr, const device_size_t size) {
+    for (device_size_t i = 0; i < size; ++i) {
         if (arr[i] >= x) return i;
     }
     return size;
 }
 
 template<class T, unsigned degree = 3>
-__device__ unsigned cooperative_linear_search(bool active, T x, const T* arr, const unsigned size) {
+__device__ unsigned cooperative_linear_search(bool active, T x, const T* arr, const device_size_t size) {
     enum { WINDOW_SIZE = 1 << degree };
 
     const unsigned my_lane_id = lane_id();
-    unsigned lower_bound = size;
     unsigned leader = WINDOW_SIZE*(my_lane_id >> degree);
+    device_size_t lower_bound = size;
     const uint32_t window_mask = __funnelshift_l(FULL_MASK, 0, WINDOW_SIZE) << leader; // TODO replace __funnelshift_l() with compile time computation
     assert(my_lane_id >= leader);
     const int lane_offset = my_lane_id - leader;
@@ -101,7 +99,7 @@ __device__ unsigned cooperative_linear_search(bool active, T x, const T* arr, co
         int key_idx = lane_offset - WINDOW_SIZE;
         const T leader_x = __shfl_sync(window_mask, x, leader);
         const T* leader_arr = reinterpret_cast<const T*>(__shfl_sync(window_mask, reinterpret_cast<uint64_t>(arr), leader));
-        const unsigned leader_size = __shfl_sync(window_mask, size, leader);
+        const device_size_t leader_size = __shfl_sync(window_mask, size, leader);
 
         const auto leader_active = __shfl_sync(window_mask, active, leader);
         bool advance = leader_active;
