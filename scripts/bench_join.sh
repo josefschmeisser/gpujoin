941b161bb8582f341e811561633a2d04d246e284
Josef Schmeißer <josef.schmeisser@tum.de>
2022-10-24 00:49:22 +0200

    fix

--- scripts/bench_join.sh
+++ scripts/bench_join.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-declare -i relation_r_size=$(2**26)
+declare -i relation_r_size=$((2**26))
 declare -i relation_s_size=0
 maxbits=( 26 28 30 32 )
 for i in "${maxbits[@]}"
 do
-    relation_s_size=$((2 ** (i - 1)))
+    relation_s_size=$((2**i))
     eval "./index_lookup -a plain -i lower_bound -l ${relation_r_size} -e ${relation_s_size} -m 64 --dataset dense"
     eval "./index_lookup -a plain -i radix_spline -l ${relation_r_size} -e ${relation_s_size} -m 64 --dataset dense"
     eval "./index_lookup -a plain -i harmonia -l ${relation_r_size} -e ${relation_s_size} -m 64 --dataset dense"
