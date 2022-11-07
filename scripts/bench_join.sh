faf2eb03942dd2dea17b1573ebe35c4f9838db21
Josef Schmeißer <josef.schmeisser@tum.de>
2022-11-07 10:07:48 +0100

    set output filename

--- scripts/bench_join.sh
+++ scripts/bench_join.sh
@@ -4,17 +4,18 @@ declare -i relation_r_size=$((2**26))
 declare -i relation_s_size=$((2**26))
 declare -i relation_s_end_size=$((2**34))
 declare -i step=$((128*(10**6)))
+declare -r output="simple_join.yml"
 
 while [ $relation_s_size -le $relation_s_end_size ]
 do
     echo "current S size: ${relation_s_size}"
-    eval "./index_lookup -a plain -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense"
-    eval "./index_lookup -a plain -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense"
-    eval "./index_lookup -a plain -i harmonia -l ${relation_r_size} -e ${relation_s_size} --dataset dense"
-    eval "./index_lookup -a plain -i btree -l ${relation_r_size} -e ${relation_s_size} --dataset dense"
-    eval "./index_lookup -a partitioning -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense"
-    eval "./index_lookup -a partitioning -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense"
-    eval "./index_lookup -a partitioning -i harmonia -l ${relation_r_size} -e ${relation_s_size} --dataset dense"
-    eval "./index_lookup -a partitioning -i btree -l ${relation_r_size} -e ${relation_s_size} --dataset dense"
+    eval "./index_lookup -a plain -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "./index_lookup -a plain -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "./index_lookup -a plain -i harmonia -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "./index_lookup -a plain -i btree -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "./index_lookup -a partitioning -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "./index_lookup -a partitioning -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "./index_lookup -a partitioning -i harmonia -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "./index_lookup -a partitioning -i btree -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
     relation_s_size=relation_s_size+step
 done
