dd46da61f1dbe48eb2aeecb3546b423dabab3358
Josef Schmeißer <josef.schmeisser@tum.de>
2023-04-30 21:40:35 +0200

    add lib/cuCollections.cmake

---
+++ lib/cuCollections.cmake
@@ -0,0 +1,30 @@
+include(ExternalProject)
+find_package(Git REQUIRED)
+
+set(CUCOLLECTIONS_PREFIX cuCollections)
+
+# Get RadixSpline
+ExternalProject_Add(
+    cucollections_src
+    PREFIX ${CUCOLLECTIONS_PREFIX}
+    GIT_REPOSITORY "https://github.com/NVIDIA/cuCollections.git"
+    TIMEOUT 10
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    UPDATE_COMMAND ""
+)
+
+ExternalProject_Get_Property(cucollections_src SOURCE_DIR)
+set(CUCOLLECTIONS_INCLUDE_DIR ${SOURCE_DIR}/include)
+file(MAKE_DIRECTORY ${CUCOLLECTIONS_INCLUDE_DIR}) # https://gitlab.kitware.com/cmake/cmake/-/issues/15052
+
+# An interface library has no source files, however, we can use it
+# to propagte include directories via INTERFACE_INCLUDE_DIRECTORIES to targets.
+# By adding such a library through target_link_libraries() to a target,
+# the library's interface include directories will be propagated to the target.
+add_library(cucollections INTERFACE)
+set_target_properties(cucollections PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CUCOLLECTIONS_INCLUDE_DIR})
+
+# Dependencies
+add_dependencies(cucollections cucollections_src)
