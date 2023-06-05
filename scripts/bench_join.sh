c72f7d4b34ad47151497abab69c4b5748d87e249
Josef Schmeißer <j.m.schmeisser@gmail.com>
2023-06-05 21:23:13 +0200

    add numactl to invocations

--- scripts/bench_join.sh
+++ scripts/bench_join.sh
@@ -9,13 +9,14 @@ declare -r output="simple_join.yml"
 while [ $relation_s_size -le $relation_s_end_size ]
 do
     echo "current S size: ${relation_s_size}"
-    eval "./index_lookup -a plain -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
-    eval "./index_lookup -a plain -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
-    eval "./index_lookup -a plain -i harmonia -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
-    eval "./index_lookup -a plain -i btree -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
-    eval "./index_lookup -a partitioning -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
-    eval "./index_lookup -a partitioning -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
-    eval "./index_lookup -a partitioning -i harmonia -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
-    eval "./index_lookup -a partitioning -i btree -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+#    eval "numactl --cpunodebind=0 ./index_lookup -a plain -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+#    eval "numactl --cpunodebind=0 ./index_lookup -a plain -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+#    eval "numactl --cpunodebind=0 ./index_lookup -a plain -i harmonia -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+#    eval "numactl --cpunodebind=0 ./index_lookup -a plain -i btree -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+#    eval "numactl --cpunodebind=0 ./index_lookup -a partitioning -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+#    eval "numactl --cpunodebind=0 ./index_lookup -a partitioning -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+#    eval "numactl --cpunodebind=0 ./index_lookup -a partitioning -i harmonia -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+#    eval "numactl --cpunodebind=0 ./index_lookup -a partitioning -i btree -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "numactl --cpunodebind=0 ./index_lookup -a hj -i no_op -l ${relation_r_size} -e ${relation_s_size} --dataset dense -p uniform_unique -o ${output}"
     relation_s_size=relation_s_size+step
 done
--- scripts/bench_search.sh
+++ scripts/bench_search.sh
@@ -9,7 +9,7 @@ declare -r output="search.yml"
 while [ $relation_s_size -le $relation_s_end_size ]
 do
     echo "current S size: ${relation_s_size}"
-    eval "./index_lookup -a partitioning -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
-    eval "./index_lookup -a partitioning -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "numactl --cpunodebind=0 ./index_lookup -a partitioning -i binary_search -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
+    eval "numactl --cpunodebind=0 ./index_lookup -a partitioning -i radix_spline -l ${relation_r_size} -e ${relation_s_size} --dataset dense -o ${output}"
     relation_s_size=relation_s_size+step
 done
