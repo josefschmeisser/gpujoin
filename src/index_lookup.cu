f2782a7cc9255a517dae017182fb816bfc3964f4
Josef Schmeißer <j.m.schmeisser@gmail.com>
2024-12-30 17:56:20 +0100

    rename file

--- src/index_lookup.cu
+++ src/index_lookup.cu
@@ -521,7 +521,7 @@ int main(int argc, char** argv) {
     } else {
         measuring_config.stdout_only = true;
     }
-    measuring_config.repetitions = 10;
+    measuring_config.repetitions = 1;
 /*
     if (config.execute_predefined_scenario) {
         execute_benchmark_scenario();
